edffc65e99e7a6e38ec935d929cd6ac79bd161c5
Brijbihari24 <55361395+Brijbihari24@users.noreply.github.com>
2021-03-17 00:26:01 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Icon from "./Components/Icon";
 
-function App() {
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Card, CardBody, Container, Col, Row, Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+
+const itemArray = new Array(9).fill("empty");
+
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [winMessage, setWinMessage] = useState("Circle wins");
+
+  const ReloadGame = () => {
+    setIsCross(false);
+    setWinMessage("");
+    itemArray.fill("empty", 0, 9);
+  };
+  const CheckIsWinner = () => {
+    //
+  };
+  const changeItem = (itemNumber) => {
+    if (winMessage) {
+      return toast(winMessage, { type: "success" });
+    }
+
+    if (itemArray[itemNumber] === "empty") {
+      itemArray[itemNumber] = isCross ? "Cross" : "Circle";
+      setIsCross(!isCross);
+    } else {
+      return toast("already filled", { type: "error" });
+    }
+    CheckIsWinner();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-5">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          {winMessage ? (
+            <div className="mb-2 mt-2">
+              <h1 className="text-primary text-uppercase text-center">
+                {winMessage}
+              </h1>
+              <Button color="success" block onClick={ReloadGame}>
+                Reload the Game
+              </Button>
+            </div>
+          ) : (
+            <h1 className="text-center text-warning">
+              {isCross ? "Cross" : "Circle"}turns
+            </h1>
+          )}
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
